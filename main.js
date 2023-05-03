a3a9d1165838eab818055f5126c2e90cb7bb00a9
Skpone <mulaterofranco@gmail.com>
2023-05-03 17:13:19 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const HOURHAND = document.querySelector('.hour-hand');
-const MINUTEHAND = document.querySelector('.minute-hand');
-const SECONDHAND = document.querySelector('.second-hand');
+const HOURHAND = document.querySelector('.hour-hand'); //aguja de la hora
+const MINUTEHAND = document.querySelector('.minute-hand'); //aguja de los minutos
+const SECONDHAND = document.querySelector('.second-hand'); //aguja de los segundos
 
 function runClock() {
     const NOW = new Date();
@@ -10,13 +10,15 @@ function runClock() {
     const MINUTE = NOW.getMinutes();
     const SECOND = NOW.getSeconds();
     
-    const HOURDEG = (HOUR % 12) * 30;
-    const MINUTEDEG = MINUTE * 6;
-    const SECONDDEG = SECOND * 6;
     
+    const HOURDEG = (HOUR % 12) * 30; //calcular grados de la aguja hora
+    const MINUTEDEG = MINUTE * 6; //calcular grados de la aguja minutos
+    const SECONDDEG = SECOND * 6;//calcular grados de la aguja segundos
+    
+    //aplicamos las rotaciones correspondientes a cada aguja
     HOURHAND.style.transform = `rotate(${HOURDEG}deg)`;
     MINUTEHAND.style.transform = `rotate(${MINUTEDEG}deg)`;
     SECONDHAND.style.transform = `rotate(${SECONDDEG}deg)`;
 }
 
-setInterval(runClock, 1000);
+setInterval(runClock, 1000); //llamamos a la funcion cada segudo
